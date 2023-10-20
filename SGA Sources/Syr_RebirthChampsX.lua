4759d850fbf0e507db7a1bb0b5bff8aadfe9b4ce
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-10-20 23:28:26 +0300

    Update Syr_RebirthChampsX.lua

--- SGA Sources/Syr_RebirthChampsX.lua
+++ SGA Sources/Syr_RebirthChampsX.lua
@@ -1,3 +1,14 @@
+pcall(function()
+    if getgenv().Loaded then
+        print("Main script has been executed")
+    else
+        print("Main script has not been executed")
+        game.Players.LocalPlayer:Kick("Unautorized UI access !")
+        
+        while true do end
+    end
+end)
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
