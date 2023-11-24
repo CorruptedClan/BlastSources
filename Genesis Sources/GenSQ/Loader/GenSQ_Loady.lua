78ad6a823b67c88057577e72448dfd734cd37eb5
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-11-24 11:27:59 +0200

    Update GenSQ_Loady.lua

--- Genesis Sources/GenSQ/Loader/GenSQ_Loady.lua
+++ Genesis Sources/GenSQ/Loader/GenSQ_Loady.lua
@@ -1,1 +1,47 @@
+function StartupScript()
+loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 
+local Window = Library.CreateLib("Genesis | SideQuest", "BloodTheme")
+
+local Tab = Window:NewTab("Games")
+
+local Discord = Tab:NewSection("https://dsc.gg/genesis-z")
+local Section = Tab:NewSection("https://www.youtibe.com/@EdxymOnline")
+
+Section:NewButton("GenSQ | Blood Surge", "in the honor of RobloxBlast", function()
+    GenBS()
+end)
+
+Section:NewButton("GenSQ | Become NBA to proveom wrong", "in the honor of RobloxBlast", function()
+    GenNBA()
+end)
+
+Section:NewButton("GenSQ | Break In 2", "in the honor of RobloxBlast", function()
+    GenBItwo()
+end)
+
+
+
+--games
+function GenBS()
+print("?")
+end
+
+function GenNBA()
+print("?")
+end
+
+function GenBItwo()
+print("?")
+end
+
+
+
+
+
+
+
+
+
+--startup end
+end
